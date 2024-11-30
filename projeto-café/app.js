67bd872c703f7eac39c19b0e37f8e4faa517b67c
LUCAS HOEPERS QUINAGLIA <lucas.hoepers.quinaglia@gmail.com>
2024-11-30 16:31:18 -0300

    sobre atualizado

--- projeto-café/app.js
+++ projeto-café/app.js
@@ -34,12 +34,27 @@ const HomePage = {
 
 const AboutPage = {
   template: `
-      <div>
+      <div class='sobre'>
         <h1 class='titulo'>Sobre a Café & Cia</h1>
         <p>A Café & Cia é uma cafeteria fictícia que oferece uma experiência única para os amantes de café.</p>
         <p>Fundada em 2024, nossa missão é trazer cafés especiais de várias partes do mundo, preparados com o maior cuidado.</p>
 
+        <h2>Nossos Valores</h2>
+        <ul id='lista-sobre'>
+          <li>Café de Qualidade</li>
+          <li>Atendimento Personalizado</li>
+          <li>Ambiente Aconchegante</li>
+          <li>Sustentabilidade</li>
+        </ul>
 
+        <h2>Nossa Equipe</h2>
+        <p>Nossa equipe é formada por baristas especializados, prontos para preparar o café perfeito para você.</p>
+        <p>Estamos sempre em busca de novos talentos, então se você ama café, venha fazer parte do nosso time!</p>
+
+        <h2>Localização</h2>
+        <p>Estamos localizados na Rua dos Cafés, 123, na cidade de Coffeeville.</p>
+        <p>Visite-nos e descubra o melhor café da região!</p>
+        <img src="./imagens/cu coffe house.png" alt="Cafeteria Café & Cia" class="cafe-image">
 
       </div>
     `
